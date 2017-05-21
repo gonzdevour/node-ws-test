1110520b95dbd3f5a4a944bee863fa07c694645e
gonzdevour <jyunru.li@gmail.com>
2017-05-21 23:02:27 +0800

    a
    a

--- index.js
+++ index.js
@@ -9,17 +9,16 @@ app.use(express.static(__dirname + "/"))
 var server = http.createServer(app)
 server.listen(port)
 
-console.log("http server listening on %d", port)
+ws.send("http server listening on %d", port)
 
 var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
+ws.send("websocket server created")
 
 wss.on("connection", function(ws) {
   var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+    ws.send("something")
 
-  console.log("websocket connection open")
+  ws.send("websocket connection open")
 
   ws.on("close", function() {
     console.log("websocket connection close")
