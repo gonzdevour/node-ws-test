a7bea204a36d52a8d09a3479445fd05f528c4de7
gonzdevour <jyunru.li@gmail.com>
2017-05-21 23:04:59 +0800

    Update index2.js

--- index2.js
+++ index2.js
@@ -26,15 +26,3 @@ wss.on("connection", function(ws) {
     clearInterval(id)
   })
 })
-
-// Broadcast to all.
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    // Broadcast to everyone else.
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-    });
-  });
-});
