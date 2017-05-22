a4a9ea54f18a534baf2e0bd3364f7625fcf82370
gonzdevour <jyunru.li@gmail.com>
2017-05-22 13:29:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ wss.on("connection", function(ws) {
     ws.send("got message from server")
     // Broadcast to everyone else.
     wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(data);
       }
     });
