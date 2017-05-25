ca6a59ac085169d5ec40a33a779867b6b09c6122
gonzdevour <jyunru.li@gmail.com>
2017-05-25 12:08:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ wss.on("connection", function(ws) {
   clients.push(ws);
   var id = setInterval(function() {
     d = JSON.stringify(new Date()), function() {  }
-    t = { "LTD":"com.playone.chat","Game":"","Pkg":"[\"Time\","+ d +"]"};
+    t = { "LTD":"com.playone.chat","Game":"","Pkg":"[\"Time\","+ d +"]","Log":"false"};
     ws.send(JSON.stringify(t), function() {  })
   }, 1000)
   
